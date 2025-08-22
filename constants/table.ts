0b9a736794f1b680c8879da2f2dbdef37b39e419
minseung-gang <strause1@nate.com>
2025-08-22 15:25:22 +0900

    feat: 사주 테이블 데이터 상수 분리 및 관리

---
+++ constants/table.ts
@@ -0,0 +1,55 @@
+export const SAJU_DATA = {
+  sibsungGan: {
+    hour: { name: '傷官', sub: '(상관)' },
+    day: { name: '比肩', sub: '(비견)' },
+    month: { name: '傷官', sub: '(상관)' },
+    year: { name: '傷官', sub: '(상관)' },
+  },
+  chungan: {
+    hour: { hangul: '임', hanja: '壬', element: '陰水' },
+    day: { hangul: '정', hanja: '丁', element: '陰火' },
+    month: { hangul: '계', hanja: '癸', element: '陰水' },
+    year: { hangul: '계', hanja: '癸', element: '陰水' },
+  },
+  jiji: {
+    hour: { hangul: '인', hanja: '寅', element: '陽木' },
+    day: { hangul: '사', hanja: '巳', element: '陰火' },
+    month: { hangul: '해', hanja: '亥', element: '陰水' },
+    year: { hangul: '유', hanja: '酉', element: '陰金' },
+  },
+  sibsungJi: {
+    hour: { name: '比肩', sub: '(비견)' },
+    day: { name: '劫財', sub: '(겁재)' },
+    month: { name: '食神', sub: '(식신)' },
+    year: { name: '偏財', sub: '(편재)' },
+  },
+  sibiunsyung: {
+    hour: { name: '死', sub: '(사)' },
+    day: { name: '帝旺', sub: '(제왕)' },
+    month: { name: '胎', sub: '(태)' },
+    year: { name: '長生', sub: '(장생)' },
+  },
+  sibisinsal: {
+    hour: { name: '劫殺', sub: '(겁살)' },
+    day: { name: '地殺', sub: '(지살)' },
+    month: { name: '驛馬殺', sub: '(역마살)' },
+    year: { name: '將星殺', sub: '(장성살)' },
+  },
+  sinsal: {
+    hour: { items: [] },
+    day: { items: [] },
+    month: { items: [{ name: '天乙', sub: '(천을귀인)' }] },
+    year: {
+      items: [
+        { name: '天乙', sub: '(천을귀인)' },
+        { name: '太極', sub: '(태극귀인)' },
+        { name: '文昌', sub: '(문창귀인)' },
+      ],
+    },
+  },
+};
+export const SAJU_TABLE_COLUMNS = ['時', '日', '月', '年'];
+export const USER_SAJU_DATE = {
+  name: '김로켓',
+  birthDateTime: '1980-08-27T08:10:00',
+};
