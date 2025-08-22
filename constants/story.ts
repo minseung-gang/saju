3cc9d26c87c0261af30d989aa9ae2af635a5fe9b
minseung-gang <strause1@nate.com>
2025-08-22 15:23:48 +0900

    feat: storyPanel 관련 메시지 상수 분리 및 관리

---
+++ constants/story.ts
@@ -0,0 +1,16 @@
+export const STORY_PANEL_MESSAGE = [
+  {
+    message: '이제 본격적으로\n OO님의 사주팔자를\n 분석해볼 차례네요.',
+    position: {
+      top: '49.5',
+      left: '35',
+    },
+  },
+  {
+    message: '제가 oo님의 사주를\n 보기 쉽게 표로 정리했어요',
+    position: {
+      top: '74.8',
+      left: '39.2',
+    },
+  },
+];
