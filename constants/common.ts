dc3d0757afb51a99d334eb94c2863c53a1544474
minseung-gang <strause1@nate.com>
2025-08-22 15:22:48 +0900

    feat: 이미지 경로 상수로 분리 및 관리

---
+++ constants/common.ts
@@ -0,0 +1,14 @@
+export const IMAGE_URL = {
+  chapter1_scene: {
+    src: '/images/chapter1-scene.png',
+    alt: '챕터 이미지01',
+  },
+  left_lainbow_cloud: {
+    src: '/images/left_rainbow_cloud.png',
+    alt: 'left_rainbow_cloud',
+  },
+  right_lainbow_cloud: {
+    src: '/images/right_rainbow_cloud.png',
+    alt: 'right_rainbow_cloud',
+  },
+};
